9885388f4416c169ee6b4d3d861af8675f0b05e3
Johan Sundström <oyasumi@gmail.com>
2017-08-12 00:27:48 +0300

    feat: first nine real login keys

--- src/membership/components/InstaLoginHack.jsx
+++ src/membership/components/InstaLoginHack.jsx
@@ -21,7 +21,15 @@ const loginPath = (location.hash.match(/^#to=([^&]+)/) || [])[1];
 
 // md5.hex(`Salted!${email.toLowerCase()}`) => login key
 const emailHashToLoginKey =
-{ '390a0e6d453de00e84013054a18f525a': 'ISrypVXEQk3E' // oyasumi+test2@gmail.com
+{ "50e31b10d783463d0aaa1100615e4f83": "azertyuiop"
+, "4b678a01bdc3b8a4bb630448fec17717": "JdliHFJB2Jgd"
+, "3187284a94d8ce35e245959f447b1edd": "12twUmDVd6TM"
+, "941e5b73c7add5da2a68b541bc4987f5": "2QMZ1g1bHAOM"
+, "b2aaad3265566bce000ea2e2440b638a": "xg0EEdUcnk5s"
+, "93a820445a36ec8c9cbebdb457304d7f": "NTCYbNPZRClC"
+, "659ca2fa7a8ba477efe3fb1ef925093b": "BUlBiR1CUZ56"
+, "dcd36c690f456d67711f2b48444b6018": "0iUurgZo4Pgq"
+, "28b8af40416e624d54aeb3239d4d8b03": "cFMmfCQmUg4n"
 };
 
 function getKeyForEmail(email) {
